3f13de4760d25d3d904d97850bcf671d3320ad82
limenghua <menghua.li@netbraintech.com>
2017-08-10 13:40:10 +0800

    websocket test

--- websocket/ws-client.js
+++ websocket/ws-client.js
@@ -4,7 +4,7 @@ const ws = new WebSocket('ws://127.0.0.1:8080');
 
 ws.on('open',function(){
     console.log('opened');
-    ws.send('client:some thing');
+    ws.send('hello');
 });
 
 ws.on('message',function(data){
--- websocket/ws-server.js
+++ websocket/ws-server.js
@@ -3,10 +3,16 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({port:8080});
 
 wss.on('connection',function (ws){
+    console.log("on connections");
+    ws.send('welcome,please give the right passport');
+
     ws.on('message',function(message){
         console.log('received: %s',message);
+
+        if(message === 'hello'){
+            ws.send('world');
+        }else{
+            ws.send('please give the right passport');
+        }       
     });
-
-    ws.send('something');
-
 });
