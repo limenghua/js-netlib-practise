0f910e37aa567c80535e2f9e66037d99aea8faf0
limenghua <menghua.li@netbraintech.com>
2017-08-11 11:23:55 +0800

    websocket test

--- websocket/ws-client.js
+++ websocket/ws-client.js
@@ -4,7 +4,9 @@ const ws = new WebSocket('ws://127.0.0.1:8080');
 
 ws.on('open',function(){
     console.log('opened');
-    ws.send('hello');
+    for(let i = 1;i<10;i++){
+        ws.send(i+' 5 hello');
+    }    
 });
 
 ws.on('message',function(data){
--- websocket/ws-echo-server.js
+++ websocket/ws-echo-server.js
@@ -2,10 +2,36 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({port:8080});
 
+let currentIndex = 1;
+
+function jugeMessage(message){
+    let items = message.split(" ");
+
+    if(items.length !== 3){
+        console.log("conn't split to 3:",message);
+    }
+
+    let index = parseInt(items[0]);
+    let length = parseInt(items[1]);
+    let msg = items[2];
+
+    if(index !== currentIndex){
+        console.log('index not match:',currentIndex,",",message);
+    }
+
+    if(length !== msg.length){
+        console.log('msg lenth not match:',length,",",message);
+    }
+}
+
 wss.on('connection',function (ws){
     console.log("on connections");
 
     ws.on('message',function(message){
-        ws.send(message);
+        jugeMessage(message);
+        currentIndex ++;
+        if(currentIndex % 10000 === 0){
+            console.log(currentIndex,":",message);
+        }
     });
 });
